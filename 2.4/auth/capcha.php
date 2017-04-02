6ec8d1ccddfe80913070d96d0d0675aabeb3a601
TillKuk <jilbiljeka@yandex.ru>
2017-04-02 16:23:44 +0500

    hw 2.4

---
+++ 2.4/auth/capcha.php
@@ -0,0 +1,29 @@
+<?php
+    $session_num = ( isset($_SESSION['number']) ? $_SESSION['number'] : 0);
+    $session_num++;
+    $_SESSION['number'] = $session_num;
+
+    $capcha_keys = [
+        'public_key' => '6LfsMRsUAAAAAAy3cHNpSfliKF31Sui745DBLuLY',
+        'secret_key' => '6LfsMRsUAAAAAHgStjPik3ZRys8yiN-tcr2E_ih-'
+    ];
+    $capcha_query = 'https://www.google.com/recaptcha/api/siteverify?secret='.$capcha_keys['secret_key'].'&response='.$_POST["g-recaptcha-response"].'&remoteip='.$_SERVER['REMOTE_ADDR'];
+    $send_capcha = file_get_contents($capcha_query);
+    $reply_capcha = json_decode($send_capcha, true);
+
+    if ( isset($_POST["g-recaptcha-response"]) )
+    {
+        $session_num_ban = ( isset($_SESSION['number_ban']) ? $_SESSION['number_ban'] : 0);
+        $session_num_ban++;
+        $_SESSION['number_ban'] = $session_num_ban;
+        $capcha_errors = [];
+        if ( $_POST["g-recaptcha-response"] == '' )
+        {
+            $capcha_errors [] = 'Заполните капчу!';
+        } elseif ( $reply_capcha["success"] )
+        {
+            $capcha_true = true;
+        } else {
+            $capcha_errors [] = 'Капча заполнена не верно';
+        }
+    }
--- 2.4/auth/login.php
+++ 2.4/auth/login.php
@@ -1,11 +1,16 @@
 <?php
     require_once ('db.php');
     require_once ('functions.php');
+    require_once ('capcha.php');
+
     if ( !empty($_SESSION['log_user']) )
     {
         redirect('u/kotyukov/2.4/index.php');
     }
-    
+    if ( $_SESSION['number_ban'] > 5 )
+    {
+
+    } else
     $data = $_POST;
     if( isset($data["go_login"]) )
     {
@@ -25,13 +30,34 @@
                 }
             }
         }
-        if ( empty($errors) and !empty($no_errors) )
+        if ( $session_num_ban === null )
         {
-            $_SESSION['log_user'] = $data;
-            redirect('u/kotyukov/2.4/index.php');
-        } elseif ( empty($no_errors) )
+            if ( empty($errors) and !empty($no_errors) )
+            {
+                $_SESSION['log_user'] = $data;
+                redirect('u/kotyukov/2.4/index.php');
+                die;
+            } elseif ( empty($no_errors) )
+            {
+                $errors [] = 'Пользователя с таким логин не существует!';
+            }
+        } else
         {
-            $errors [] = 'Пользователя с таким логин не существует!';
+            if ( empty($capcha_errors) )
+            {
+                if ( empty($errors) and !empty($no_errors) )
+                {
+                    if ( $capcha_true )
+                    {
+                        $_SESSION['log_user'] = $data;
+                        redirect('u/kotyukov/2.4/index.php');
+                        die;
+                    }
+                }else
+                {
+                    $errors [] = 'Пользователя с таким логин не существует!';
+                } 
+            }
         }
     }
 ?>
@@ -44,17 +70,28 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
     <link href="/u/kotyukov/2.4/css/style.css" rel="stylesheet">
+    <script src="https://www.google.com/recaptcha/api.js"></script>
     <title>Домашние задание 2.4</title>
 </head>
 <body>
 <div class="form">
 
-    <?php
-    if (!empty($errors)) 
+    <?php if ( $_SESSION['number_ban'] > 5): ?>
+        <p style="color: red;">У меня не хватила сил сделать, что бы бан был на определённое время.<br /> По этому вы условно заблокированы!</p>
+        <form action="logout.php" method="POST">
+            <input type="submit" value="Снять бан">
+        </p>
+        </form>
+    <?php   
+        die;
+    endif;
+    if ( !empty($errors) ) 
     {
         echo '<p style="color: red;">'.array_shift($errors).'</p>';
     }
-    ?>    
+    ?> 
+
+    <?php if( $_SESSION['number'] < 6 ): ?>   
     <form action="login.php" method="POST">
         <p>
             <lable id="login">Логин:</lable><br />
@@ -68,6 +105,44 @@
             <input type="submit" name="go_login" value="Войти">
         </p>
     </form>
+    <?php endif; ?>
+
+    <?php if( $_SESSION['number'] >= 6 ):
+        switch ( $session_num_ban ) 
+        {
+            case 1:
+                echo '<p>У Вас осталось '.(6-$session_num_ban).' попыток</p>';
+                break;
+            case 2:
+                echo '<p>У Вас осталось '.(6-$session_num_ban).' попытки</p>';
+                break;
+            case 3:
+                echo '<p style="color: red;">У Вас осталось '.(6-$session_num_ban).' попытки</p>';
+                break;
+            case 4:
+                echo '<p style="color: red;">У Вас осталось '.(6-$session_num_ban).' попытки</p>';
+                break;
+            case 5:
+                echo '<p style="color: red;">У Вас осталось '.(6-$session_num_ban).' попытка</p>';
+                break;
+        }
+    ?>    
+
+    <form action="login.php" method="POST">
+        <p>
+            <lable id="login">Логин:</lable><br />
+            <input id="login" type="text" name="login" value="<?= @$data["login"] ?>" placeholder="admin">
+        </p>
+        <p>
+            <lable id="password">Пароль:</lable><br />
+            <input id="password" type="password" name="password">
+        </p>
+        <p>
+            <input type="submit" name="go_login" value="Войти">
+        </p>
+        <div class="g-recaptcha" data-sitekey="6LfsMRsUAAAAAAy3cHNpSfliKF31Sui745DBLuLY"></div>
+    </form>
+    <?php endif; ?>
 
 </div>
 </body>
