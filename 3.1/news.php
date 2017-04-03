f901e1178d09f377759b418b70e732c1a3cdbf7c
TillKuk <jilbiljeka@yandex.ru>
2017-04-03 19:08:33 +0500

    hw 3.1

--- 3.1/news.php
+++ 3.1/news.php
@@ -34,15 +34,16 @@
 		public function printNews()
 		{
 			$commit = new CommetClass('commet_news.json');
+			$commetnArray = $commit->printCommet();
 			$uriJson = $this->uri;
 			$arrayNewsJson = file_get_contents('./news_json/'.$uriJson);
             $arrayNews = json_decode($arrayNewsJson);
             for ($i=0; $i < 5; $i++) 
             { 
-            	echo '<div><h1>'.$arrayNews->{news.$i}->h1.'</h1>';
-            	echo '<p class="commit">Commet: '.($commit->printCommet())[$i].'</p>';
-	            echo '<p>'.$arrayNews->{news.$i}->p1.'</p>';
-	           	echo '<p>'.$arrayNews->{news.$i}->p2.'</p></div>';
+            	echo '<div><h1>'.$arrayNews->{'news'.$i}->h1.'</h1>';
+            	echo '<p class="commit">Commet: '.$commetnArray [$i].'</p>';
+	            echo '<p>'.$arrayNews->{'news'.$i}->p1.'</p>';
+	           	echo '<p>'.$arrayNews->{'news'.$i}->p2.'</p></div>';
             }
 
 		}
