75a2f8bbf64306ad8f155cf63cb544540b055b43
TillKuk <jilbiljeka@yandex.ru>
2017-04-03 19:36:16 +0500

    hw 3.1

--- 3.1/news.php
+++ 3.1/news.php
@@ -12,13 +12,13 @@
 		{
 			$uriJson = $this->uri;
 			$arrayCommetJson = file_get_contents('./news_json/'.$uriJson);
-            $arrayCommet = json_decode($arrayCommetJson);
-            $arrayCommetPrint = [];
-            for ($i=0; $i < 5; $i++) 
-            { 
-	            $arrayCommetPrint[] = $arrayCommet->{$i};
-            }
-            return $arrayCommetPrint;
+			$arrayCommet = json_decode($arrayCommetJson);
+			$arrayCommetPrint = [];
+			for ($i=0; $i < 5; $i++) 
+			{ 
+				$arrayCommetPrint[] = $arrayCommet->{$i};
+			}
+			return $arrayCommetPrint;
 		}
 	}
 
@@ -37,14 +37,14 @@
 			$commetnArray = $commit->printCommet();
 			$uriJson = $this->uri;
 			$arrayNewsJson = file_get_contents('./news_json/'.$uriJson);
-            $arrayNews = json_decode($arrayNewsJson);
-            for ($i=0; $i < 5; $i++) 
-            { 
-            	echo '<div><h1>'.$arrayNews->{'news'.$i}->h1.'</h1>';
-            	echo '<p class="commit">Commet: '.$commetnArray [$i].'</p>';
-	            echo '<p>'.$arrayNews->{'news'.$i}->p1.'</p>';
-	           	echo '<p>'.$arrayNews->{'news'.$i}->p2.'</p></div>';
-            }
+			$arrayNews = json_decode($arrayNewsJson);
+			for ($i=0; $i < 5; $i++) 
+			{
+				echo '<div><h1>'.$arrayNews->{'news'.$i}->h1.'</h1>';
+				echo '<p class="commit">Commet: '.$commetnArray [$i].'</p>';
+				echo '<p>'.$arrayNews->{'news'.$i}->p1.'</p>';
+				echo '<p>'.$arrayNews->{'news'.$i}->p2.'</p></div>';
+			}
 
 		}
 	}
