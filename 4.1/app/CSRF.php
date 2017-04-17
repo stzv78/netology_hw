d3ccba6fef5af3e6db76f11226b3bf142a7d1858
Oljinol <jilbiljeka@yandex.ru>
2017-04-17 20:23:48 +0500

    hw 4.1

--- 4.1/app/CSRF.php
+++ 4.1/app/CSRF.php
@@ -1,10 +1,12 @@
 <?php
 require_once 'session.php';
-
+$_SESSION['csrfTokenPost'] = isset($_SESSION['csrfTokenPost']) ? $_SESSION['csrfTokenPost'] : '';
+$_SESSION['csrfToken'] = isset($_SESSION['csrfToken']) ? $_SESSION['csrfToken'] : '';
+$_POST['csrf'] = isset($_POST['csrf']) ? $_POST['csrf'] : '';
 $csrfToken = '';
 if (empty($_POST['csrf']) || isset($_POST['inquiry'])) {
     $_SESSION['csrfTokenPost'] = $_SESSION['csrfToken'];
-    if (version_compare(PHP_VERSION, '7.0.0', '<=')) {
+    if (version_compare(PHP_VERSION, '7.0.0', '>=')) {
         $csrfToken = bin2hex(random_bytes(32));
     } else {
         if (function_exists('mcrypt_create_iv')) {
@@ -14,4 +16,24 @@ if (empty($_POST['csrf']) || isset($_POST['inquiry'])) {
         }
     }
     $_SESSION['csrfToken'] = $csrfToken;
+}
+
+if(!function_exists('hash_equals')) {
+    function hash_equals($str1, $str2)
+    {
+        if(strlen($str1) != strlen($str2))
+        {
+            return false;
+        }
+        else
+        {
+            $res = $str1 ^ $str2;
+            $ret = 0;
+            for($i = strlen($res) - 1; $i >= 0; $i--)
+            {
+                $ret |= ord($res[$i]);
+            }
+            return !$ret;
+        }
+    }
 }
--- 4.1/app/db.php
+++ 4.1/app/db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO("mysql:host=localhost;dbname=global;charset=utf8", "root", "root", [
+    $db = new PDO("mysql:host=localhost;dbname=global;charset=utf8", "kotyukov", "neto0988", [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 } catch (PDOException $exception) {
